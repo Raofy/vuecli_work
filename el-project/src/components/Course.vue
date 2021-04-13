5b9f59a1d3261d88453c8f03909b297e0d278da6
fuyi <1312170600@qq.com>
2021-04-14 00:25:43 +0800

    查询提交

--- el-project/src/components/Course.vue
+++ el-project/src/components/Course.vue
@@ -1,4 +1,16 @@
 <template>
+<div>
+  <el-row :gutter="10">
+    <el-col :span="5">
+      <el-input v-model="filter.course_name" placeholder="请输入内容">
+        <i slot="prefix" class="el-input__icon el-icon-search"></i>
+      </el-input>
+    </el-col>
+    <el-col :span="1">
+      <el-button type="primary" @click="search">查询</el-button>
+    </el-col>
+  </el-row>
+      
       <el-table :data="courseList" style="width: 100%" border height="550" v-loading="loading" element-loading-text="数据加载中。。。。">
         <el-table-column fixed="left" prop="id" label="ID" ></el-table-column>
         <el-table-column prop="course_name" label="课程名称"></el-table-column>
@@ -6,22 +18,21 @@
         <el-table-column prop="sort_num" label="排序"></el-table-column>
         <el-table-column prop="status" label="状态"></el-table-column>
       </el-table>
+</div>
 </template>
 
 <script>
 export default {
-    // data() {
-    //   const item = {
-    //     id: '1',
-    //     course_name: 'Java',
-    //     price: '518',
-    //     sort_num: 1,
-    //     status: "上架"
-    //   };
-    //   return {
-    //     courseList: Array(1).fill(item)
-    //   }
-    // },
+    data() {
+      return {
+        // 定义数据
+        loading: false,
+        courseList: [],
+        filter: {
+          course_name: ""
+        }
+      }
+    },
 
     // create()定义钩子函数，在DOM页面之前执行
     created() {
@@ -39,6 +50,21 @@ export default {
                 methodName: "findCourseList"
               }
             }).then((response) => {
+              // 将获取的数据进行渲染进去
+              this.courseList = response.data
+              this.loading = false
+            });
+        },
+        search() {
+          this.loading = true;
+          const url = "http://localhost:8080/lagou_edu_home/course";
+          this.axios.get(url, {
+            params: {
+              methodName: "findByCourseNameAndStatus",
+              course_name: this.filter.course_name,
+              status: 0
+            }
+          }).then((response) => {
               // 将获取的数据进行渲染进去
               this.courseList = response.data
               this.loading = false
